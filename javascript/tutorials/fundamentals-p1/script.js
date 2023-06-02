312a8750f19ecb13a45c6480ec5015168c6d9dd9
BryceVerberne <bryceverberne@gmail.com>
2023-06-01 17:38:21 -0700

    ✨ Add JavaScript from HTML

--- javascript/tutorials/fundamentals-p1/script.js
+++ javascript/tutorials/fundamentals-p1/script.js
@@ -1,1 +1,34 @@
-console.log("This is an external Hello World!");
+// We are printing "Hello, World!" to console
+console.log("Hello, World!");
+
+let message; // We are creating a variable
+message = 'Hooga booga!'; // We are storing 'Hello' in `message`. 
+                          // Notice that single quotes, and that
+                          // we don't have to specify it as type string.
+                
+alert(message); // This displays our variable
+        
+// The following are questions answered from the Variables section of our lesson.
+// Task 1: Working with variables
+//  1. Declare two variables: `admin` and `name`. 
+let admin;
+let person;
+        
+//  2. Assign the value "John" to `name` and copy that value from `name` to `admin`.
+person = 'John';
+admin = person;
+        
+//  3. Show the value of `admin` using `alert`
+alert(admin);
+        
+// Task 2: Giving the right name
+//  1. Create a variable with the name of our planet.
+let earth;
+        
+//  2. Create a name to store the name of a current visitor.
+let currentVisitorName;
+        
+// Task 3: Uppercase const?
+const BIRTHDAY = '18.04.1982';  // The value is known upon declaration; therefore, it's uppercase.
+const age = BIRTHDAY;           // This is not uppercase since the value of `age` is not
+                                // known upon declaration but is calculated in run time.
