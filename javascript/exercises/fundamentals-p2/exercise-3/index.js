538d887f0cb77b7f5268a77a8dfb89eb4c6f36cb
BryceVerberne <bryceverberne@gmail.com>
2023-06-06 07:28:54 -0700

    🌱 Start new math JS exercise

---
+++ javascript/exercises/fundamentals-p2/exercise-3/index.js
@@ -0,0 +1,55 @@
+/**
+ * Lets do some math!
+ * Some rules first:
+ *   Enter the operations, replacing the `"?"`, make the computer do the work for you. 
+ *   Do not manually enter the answer, for example: "one plus ten" would look like 1+10
+ * 
+ * a = one plus eight
+ * b = 22 times three
+ * c = the *remainder* of 5/4
+ * d = the variable 'a' minus 17
+ * e = the sum of the previous four variables
+ */
+// ==============
+const a = "?"
+const b = "?"
+const c = "?"
+const d = "?"
+const e = "?"
+// ==============
+
+// TESTING
+if (a === 9) {
+  console.log("'a' is correct!")
+} else {
+  console.log(`'a' is incorrect, got ${a}, expected 9`)
+}
+console.log("\n");
+
+if (b === 66) {
+  console.log("'b' is correct!")
+} else {
+  console.log(`'b' is incorrect, got ${b}, expected 66`)
+}
+console.log("\n");
+
+if (c === 1) {
+  console.log("'c' is correct!")
+} else {
+  console.log(`'c' is incorrect, got ${c}, expected 1`)
+}
+console.log("\n");
+
+if (d === -8) {
+  console.log("'d' is correct!")
+} else {
+  console.log(`'d' is incorrect, got ${d}, expected -8`)
+}
+console.log("\n");
+
+if (e === 68) {
+  console.log("'e' is correct!")
+  console.log("\n Congrats! You may move onto the next exercise")
+} else {
+  console.log(`'e' is incorrect, got ${e}, expected 68`)
+}
