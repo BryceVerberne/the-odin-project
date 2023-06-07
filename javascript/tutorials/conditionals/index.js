24a509d24a8eb3d6b59559d88c9917abf0e934d9
BryceVerberne <bryceverberne@gmail.com>
2023-06-07 10:14:03 -0700

    🔄 Rewrite the "if" to "switch"

--- javascript/tutorials/conditionals/index.js
+++ javascript/tutorials/conditionals/index.js
@@ -46,3 +46,22 @@ if (a == 2 || a == 3) {
   alert( '2,3' );
 }
 */
+let a = +prompt('a?', '');
+
+switch (a) {
+    case 0:
+        alert(0);
+        break;
+    
+    case 1:
+        alert(1);
+        break;
+    
+    case 2:
+    case 3:
+        alert('2,3');
+        break;
+
+    default:
+        alert("No options available.");
+}
