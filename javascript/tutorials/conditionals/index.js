3d86ebdf5ad0a8e2297e65c6d01788e4fa5b9dca
BryceVerberne <bryceverberne@gmail.com>
2023-06-07 09:55:07 -0700

    📝 Rewrite first block of code

--- javascript/tutorials/conditionals/index.js
+++ javascript/tutorials/conditionals/index.js
@@ -17,6 +17,18 @@ switch (browser) {
     alert( 'We hope that this page looks ok!' );
 }
 */
+let browser = prompt("What browser are you using?");
+
+if (browser === "Edge") {
+    alert("You've got the Edge!");
+}
+else if ((browser === "Chrome") || (browser === "Firefox") || (browser === "Safari") || (browser === "Opera")) {
+    alert("Okay we support these browsers too");
+}
+else {
+    alert("We hope that this page looks ok!");
+}
+
 
 /*
 Rewrite the "if" to "switch".
