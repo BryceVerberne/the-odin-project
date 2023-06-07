985110ceaf686c1e72f8d0e7d1128fefe3b594c6
BryceVerberne <bryceverberne@gmail.com>
2023-06-07 09:44:36 -0700

    🌱 Add new JS conditional tutorial

---
+++ javascript/tutorials/conditionals/index.js
@@ -0,0 +1,36 @@
+/*
+Rewrite the "switch" into an "if".
+
+switch (browser) {
+  case 'Edge':
+    alert( "You've got the Edge!" );
+    break;
+
+  case 'Chrome':
+  case 'Firefox':
+  case 'Safari':
+  case 'Opera':
+    alert( 'Okay we support these browsers too' );
+    break;
+
+  default:
+    alert( 'We hope that this page looks ok!' );
+}
+*/
+
+/*
+Rewrite the "if" to "switch".
+
+let a = +prompt('a?', '');
+
+if (a == 0) {
+  alert( 0 );
+}
+if (a == 1) {
+  alert( 1 );
+}
+
+if (a == 2 || a == 3) {
+  alert( '2,3' );
+}
+*/
