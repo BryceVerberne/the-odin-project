af1709d842ddf0ef7e2517ab780f4808d74e8bba
BryceVerberne <bryceverberne@gmail.com>
2023-06-08 11:09:19 -0700

    🌱 Begin arrow function tutorial

---
+++ javascript/tutorials/arrow-functions/script.js
@@ -0,0 +1,19 @@
+/*
+    Convert the following functions to arrow functions.
+
+    function sum(a, b) {
+        return a + b;
+    }
+
+    function isPositive(number) {
+        return number >= 0;
+    }
+
+    function randomNumber() {
+       return Math.random;
+    }
+
+    document.addEventListener('click', function() {
+        console.log('Click');
+    });
+*/
