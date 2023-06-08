6750ecffa6d8edf339218fc84eeb2b569e599751
BryceVerberne <bryceverberne@gmail.com>
2023-06-07 18:23:59 -0700

    ✅ Solve question 1

--- javascript/exercises/functions/index.js
+++ javascript/exercises/functions/index.js
@@ -1,16 +1,25 @@
 /*
+    1.)
     Write a function called `add7` that takes one number and returns that 
     number + 7.
 */
+function add7(number) {
+    return Number(number) + 7;  // We can also use the `+` unary operator
+}
 
+const userNumber = prompt("Gimme a number!");
+
+console.log(add7(userNumber));
 
 /*
+    2.)
     Write a function called `multiply` that takes 2 numbers and returns their
     product.
 */
 
 
 /*
+    3.)
     Write a function called `capitalize` that takes a string and returns that
     string with only the first letter capitalized. Make sure that it can take
     strings that are lowercase, uppercase, or both. 
@@ -18,6 +27,7 @@
 
 
 /*
+    4.)
     Write a function called `lastLetter` that takes a string and returns the
     very last letter of that string.
 */
