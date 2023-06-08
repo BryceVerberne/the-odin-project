ed5ec54871c88bd8b4ebf97434f1fe07dfec813c
BryceVerberne <bryceverberne@gmail.com>
2023-06-07 19:07:37 -0700

    🔧 Solve second problem & improve output

--- javascript/exercises/functions/index.js
+++ javascript/exercises/functions/index.js
@@ -3,19 +3,31 @@
     Write a function called `add7` that takes one number and returns that 
     number + 7.
 */
-function add7(number) {
-    return Number(number) + 7;  // We can also use the `+` unary operator
+function add7(userNumber) {
+    return Number(userNumber) + 7;  // We can also use the `+` unary operator
 }
 
-const userNumber = prompt("Gimme a number!");
+alert("Provide me with a number and I'll add 7 to it.")
+const userNumber = prompt("Provide a number.");
+let result = add7(userNumber);
 
-console.log(add7(userNumber));
+alert(`${userNumber} + 7 = ${result}`)
 
 /*
     2.)
     Write a function called `multiply` that takes 2 numbers and returns their
     product.
 */
+function multiply(numOne, numTwo) {
+    return numOne * numTwo;
+} 
+
+alert("Provide me with two numbers and I'll give you their product.");
+const numOne = prompt("Value One");
+const numTwo = prompt("Value Two");
+result = multiply(numOne, numTwo);
+
+alert(`${numOne} * ${numTwo} = ${result}`);
 
 
 /*
