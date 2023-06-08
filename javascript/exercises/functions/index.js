eab24536e0cdc89591d4fbed033f47e076f54c18
BryceVerberne <bryceverberne@gmail.com>
2023-06-07 18:18:50 -0700

    🌱 Begin JS exercise on functions

---
+++ javascript/exercises/functions/index.js
@@ -0,0 +1,23 @@
+/*
+    Write a function called `add7` that takes one number and returns that 
+    number + 7.
+*/
+
+
+/*
+    Write a function called `multiply` that takes 2 numbers and returns their
+    product.
+*/
+
+
+/*
+    Write a function called `capitalize` that takes a string and returns that
+    string with only the first letter capitalized. Make sure that it can take
+    strings that are lowercase, uppercase, or both. 
+*/
+
+
+/*
+    Write a function called `lastLetter` that takes a string and returns the
+    very last letter of that string.
+*/
