58f68b5910c276b2b0a172ee3c05daf4bb384ff0
BryceVerberne <bryceverberne@gmail.com>
2023-06-10 16:01:38 -0700

    ✨ Add a few useful features

--- javascript/tutorials/event-listeners/script.js
+++ javascript/tutorials/event-listeners/script.js
@@ -1,3 +1,15 @@
 const grandparent = document.querySelector(".grandparent");
 const parent = document.querySelector(".parent");
 const child = document.querySelector(".child");
+
+function printHi() {
+    console.log("Hi");
+}
+
+grandparent.addEventListener('click', printHi);
+
+setTimeout(() => grandparent.removeEventListener('click', printHi), 2000);
+
+parent.addEventListener('click', e => console.log("Parent"), {capture: true});
+child.addEventListener('click', e => console.log("Child"), {once: true});
+document.addEventListener('click', e => console.log("Document"));
