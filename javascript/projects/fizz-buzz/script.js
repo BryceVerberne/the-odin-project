1988b518ee403f33ed5a410bd139432bb3ee0f14
BryceVerberne <bryceverberne@gmail.com>
2023-06-11 13:11:06 -0700

    🌱 Start FizzBuzz mini algorithm project

---
+++ javascript/projects/fizz-buzz/script.js
@@ -0,0 +1,10 @@
+/*
+    What is FizzBuzz? 
+    FizzBuzz is a group word game for children to teach them about division.
+    Players take turns to count incrementally, replacing any number divisible
+    by three with the word "fizz", and any number divisible by five with the
+    word "buzz", and any number divisible by both 3 and 5 with the word 
+    "fizzbuzz".
+*/
+
+// Task: Write a program that outputs the first 100 FizzBuzz numbers.
