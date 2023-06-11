90f8cddee4caade2f9a422b65326a5d4bc444da2
BryceVerberne <bryceverberne@gmail.com>
2023-06-11 13:11:50 -0700

    🧮 Solve FizzBuzz algorithm

--- javascript/projects/fizz-buzz/script.js
+++ javascript/projects/fizz-buzz/script.js
@@ -8,3 +8,17 @@
 */
 
 // Task: Write a program that outputs the first 100 FizzBuzz numbers.
+for (let i = 1; i <= 100; ++i) {
+    if (((i % 3) === 0) && ((i % 5) === 0)) {
+        console.log(`${i} Fizz, Buzz`);
+    }
+    else if ((i % 3) === 0) {
+        console.log(`${i} Fizz`);
+    }
+    else if ((i % 5) === 0) {
+        console.log(`${i} Buzz`);
+    }
+    else {
+        console.log(`${i}`);
+    }
+}
