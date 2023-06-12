f39dbe330da65d0d11458e137d37984f60958370
BryceVerberne <bryceverberne@gmail.com>
2023-06-12 13:44:48 -0700

    🎮 Make function to prompt player for choice

--- javascript/projects/rock-paper-scissors/script.js
+++ javascript/projects/rock-paper-scissors/script.js
@@ -1,14 +1,25 @@
-// Randomly return either 'Rock', 'Paper', or 'Scissors' for computer's choice. 
+let computerSelection;
+let playerSelection;
+
+// Randomly return either 'rock', 'paper', or 'scissors' for computer's choice. 
 function getComputerChoice() {
     let randomNum = Math.floor(Math.random() * 3); // Random number between 0, 1, or 2
 
     if (randomNum === 1) {
-        return "rock";
+        computerSelection = "rock";
     }
     else if (randomNum === 2) {
-        return "paper";
+        computerSelection = "paper";
     }
     else {
-        return "scissors";
+        computerSelection = "scissors";
     }
 }
+
+// Prompt player for choice of either 'rock', 'paper', or 'scissors'
+function getPlayerChoice() {
+    playerSelection = prompt("Choose your weapon!").toLowerCase();
+}
+
+getComputerChoice();
+getPlayerChoice();
