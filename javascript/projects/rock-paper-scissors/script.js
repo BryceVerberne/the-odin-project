8ee128f843edf52e7bb15c400e5d644153655d5a
BryceVerberne <bryceverberne@gmail.com>
2023-06-12 13:55:24 -0700

    ⚔️ Add dramatic introduction with alerts

--- javascript/projects/rock-paper-scissors/script.js
+++ javascript/projects/rock-paper-scissors/script.js
@@ -6,20 +6,27 @@ function getComputerChoice() {
     let randomNum = Math.floor(Math.random() * 3); // Random number between 0, 1, or 2
 
     if (randomNum === 1) {
-        computerSelection = "rock";
+        return "rock";
     }
     else if (randomNum === 2) {
-        computerSelection = "paper";
+        return "paper";
     }
     else {
-        computerSelection = "scissors";
+        return "scissors";
     }
 }
 
 // Prompt player for choice of either 'rock', 'paper', or 'scissors'
 function getPlayerChoice() {
-    playerSelection = prompt("Choose your weapon!").toLowerCase();
+    return prompt("Choose your weapon!").toLowerCase();
 }
 
-getComputerChoice();
-getPlayerChoice();
+alert("In a world where the balance of power is determined by the simple game of Rock, Paper, Scissors...");
+alert("Two formidable opponents are about to face off in a battle of wits, strategy, and chance...");
+alert("One, a mere mortal, armed with intuition and free will. The other, a relentless machine, devoid of emotion and driven by cold, hard logic...");
+alert("In this epic showdown, only one can emerge victorious. Will it be the human spirit or the precision of the machine? The first to claim five victories will be declared the champion.");
+alert("Prepare yourself, brave player. The fate of the world rests in your hands. Choose wisely... The game of Rock, Paper, Scissors begins NOW!");
+
+computerSelection = getComputerChoice();
+playerSelection = getPlayerChoice();
+
