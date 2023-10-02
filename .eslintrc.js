ade225dca8dc5d681248fc0f09479c9729d72df5
BryceVerberne <bryceverberne@gmail.com>
2023-10-02 11:04:08 -0700

    🔧 Configure ESLint for Node & Jest environments

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,13 +1,22 @@
 module.exports = {
     "env": {
         "browser": true,
-        "es2021": true
+        "es2021": true,
+        "node": true,  // Added node environment
+        "jest": true   // Added jest environment
     },
-    "extends": "eslint:recommended",
+    "extends": [
+        "eslint:recommended",
+        "plugin:jest/recommended"  // Consider adding this line after installing eslint-plugin-jest
+    ],
     "parserOptions": {
         "ecmaVersion": "latest",
         "sourceType": "module"
     },
+    "plugins": [
+        "jest"  // Consider adding this line after installing eslint-plugin-jest
+    ],
     "rules": {
+        // You can add or override rules here
     }
 }
