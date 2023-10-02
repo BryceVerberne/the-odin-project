4f18b4b73ac522f7f13369d2288f34f86f565663
BryceVerberne <bryceverberne@gmail.com>
2023-10-01 20:04:02 -0700

    🌱 Add new exercises to practice JS

---
+++ javascript/exercises/fundamentals-p4/temp-conversion/src/tempConversion.js
@@ -0,0 +1,11 @@
+const convertToCelsius = function() {
+};
+
+const convertToFahrenheit = function() {
+};
+
+// Do not edit below this line
+module.exports = {
+  convertToCelsius,
+  convertToFahrenheit
+};
