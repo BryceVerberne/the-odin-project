9465cee612fcd39ac2901bfb9348814fe9aa972e
BryceVerberne <bryceverberne@gmail.com>
2023-10-02 14:41:12 -0700

    ✅ Pass first test at basic element removal

--- javascript/exercises/fundamentals-p4/remove-from-array/src/removeFromArray.js
+++ javascript/exercises/fundamentals-p4/remove-from-array/src/removeFromArray.js
@@ -1,5 +1,13 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, element) {
+    // Search array for specified element and, if found, remove it
+    for (let i = 0; i < array.length; ++i) {
+        if (array[i] == element) {
+            array.splice(i,1);
+            return array;
+        }
+    }
 
+    return array;
 };
 
 // Do not edit below this line
