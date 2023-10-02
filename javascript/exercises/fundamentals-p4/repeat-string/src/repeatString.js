34af6458bd2ac5332b955998e1d2c208466419a1
BryceVerberne <bryceverberne@gmail.com>
2023-10-02 13:46:42 -0700

    🚀 Complete Repeat String exercise

--- javascript/exercises/fundamentals-p4/repeat-string/src/repeatString.js
+++ javascript/exercises/fundamentals-p4/repeat-string/src/repeatString.js
@@ -1,5 +1,18 @@
-const repeatString = function() {
+const repeatString = function(string, repetitions) {
+    let newString = '';
 
+    // Check to see if the number of repetitions is negative
+    if (repetitions < 0) {
+        return 'ERROR';
+    }
+    else { 
+        // Concatenate the user's string with a new string n times.
+        for (let i = 0; i < repetitions; ++i) {
+            newString += string;
+        }
+    }
+
+    return newString;
 };
 
 // Do not edit below this line
