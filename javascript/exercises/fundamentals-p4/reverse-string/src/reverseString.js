3589bff7142c58b94ae153e948806340aa4721d5
BryceVerberne <bryceverberne@gmail.com>
2023-10-02 14:16:00 -0700

    🚀 Complete Reverse String exercise

--- javascript/exercises/fundamentals-p4/reverse-string/src/reverseString.js
+++ javascript/exercises/fundamentals-p4/reverse-string/src/reverseString.js
@@ -1,6 +1,15 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    let newString = '';
 
+    // Traverse string backwards and concatenate each element to a new string
+    for (let i = string.length - 1; i >= 0; --i) {
+        newString += string[i];
+    }
+
+    return newString;
 };
+
+console.log(reverseString('hello there')); // returns 'ereht olleh'
 
 // Do not edit below this line
 module.exports = reverseString;
