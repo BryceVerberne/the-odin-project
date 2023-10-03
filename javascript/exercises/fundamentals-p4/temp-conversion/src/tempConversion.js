dac48093bd93dec2a0dbb1d2d0b6df4151e730f9
BryceVerberne <bryceverberne@gmail.com>
2023-10-03 11:41:05 -0700

    🚀 Complete `Temp Conversion` exercise

--- javascript/exercises/fundamentals-p4/temp-conversion/src/tempConversion.js
+++ javascript/exercises/fundamentals-p4/temp-conversion/src/tempConversion.js
@@ -1,8 +1,5 @@
-const convertToCelsius = function() {
-};
-
-const convertToFahrenheit = function() {
-};
+const convertToCelsius = temperature => Math.round(((temperature - 32) * (5/9)) * 10) / 10;
+const convertToFahrenheit = temperature => Math.round((temperature * (9/5) + 32) * 10) / 10;
 
 // Do not edit below this line
 module.exports = {
