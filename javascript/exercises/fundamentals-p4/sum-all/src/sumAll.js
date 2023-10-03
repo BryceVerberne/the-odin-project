06bab4f09752fe43c790a9ab4c8f44d7e3774c82
BryceVerberne <bryceverberne@gmail.com>
2023-10-02 21:17:57 -0700

    🚀 Complete Sum All exercise

--- javascript/exercises/fundamentals-p4/sum-all/src/sumAll.js
+++ javascript/exercises/fundamentals-p4/sum-all/src/sumAll.js
@@ -1,5 +1,22 @@
-const sumAll = function() {
+const sumAll = function(lower, upper) {
+    if ((lower < 0) || (upper < 0) || (typeof(lower) != 'number') || (typeof(upper) != 'number')) {
+        return 'ERROR';
+    }
+    
+    let sum = 0;
 
+    if (lower > upper) {
+        for (let i = upper; i <= lower; ++i) {
+            sum += i;
+        }
+    }
+    else {
+        for (let i = lower; i <= upper; ++i) {
+            sum += i;
+        }
+    }
+
+    return sum;
 };
 
 // Do not edit below this line
