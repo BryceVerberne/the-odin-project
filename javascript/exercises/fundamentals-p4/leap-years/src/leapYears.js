00d0f822baee3d81ea3e9821285510e6ced4030f
BryceVerberne <bryceverberne@gmail.com>
2023-10-03 10:48:17 -0700

    🚀 Complete `Leap Years` exercise

--- javascript/exercises/fundamentals-p4/leap-years/src/leapYears.js
+++ javascript/exercises/fundamentals-p4/leap-years/src/leapYears.js
@@ -1,6 +1,11 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+    if (((year % 4) === 0) && (((year % 100) !== 0) || ((year % 400)) === 0)) {
+        return true;
+    }
+    return false;
 };
+
+console.log(leapYears(2000));
 
 // Do not edit below this line
 module.exports = leapYears;
