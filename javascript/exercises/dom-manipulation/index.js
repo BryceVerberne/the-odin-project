8ee2b898c56dd98c5e8f9cc6fd7828fee128400a
BryceVerberne <bryceverberne@gmail.com>
2023-10-14 10:33:51 -0700

    ✨ Create new exercise on DOM manipulation

---
+++ javascript/exercises/dom-manipulation/index.js
@@ -0,0 +1,17 @@
+/*
+Add the following to the DOM:
+<div class="content">
+    This is the glorious text-content!
+</div>
+*/
+
+
+/*
+Make the following modifications to the DOM:
+1. A <p> with red text that says “Hey I’m red!”
+2. An <h3> with blue text that says “I’m a blue h3!”
+3. A <div> with a black border and pink background color with the following elements inside of it:
+  - Another <h1> that says “I’m in a div”
+  - A <p> that says “ME TOO!”
+Hint for this one: after creating the <div> with createElement, append the <h1> and <p> to it before adding it to the container.
+*/
