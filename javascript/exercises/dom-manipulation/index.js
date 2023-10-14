c3dd58ed9c82bf63db9db7d683eb37af0d727b6a
BryceVerberne <bryceverberne@gmail.com>
2023-10-14 11:15:03 -0700

    🪆 Add elements to a DOM element within container

--- javascript/exercises/dom-manipulation/index.js
+++ javascript/exercises/dom-manipulation/index.js
@@ -9,6 +9,7 @@ Add the following to the container class DOM:
 // Get the reference to the parent node & create the new element
 const container = document.getElementById("container");
 let content = document.createElement("div");
+let item;
 
 // Add a class & text to the new element
 content.className = "content";
@@ -49,4 +50,25 @@ content.textContent = "I'm a blue h3!";
 content.style.color = "blue";
 
 // Append the element to the container parent node
+container.appendChild(content);
+
+
+// Problem 3.
+// Create the div & h1 element
+content = document.createElement("div");
+item = document.createElement("h1");
+
+// Add styling to the parent & child nodes
+content.setAttribute("style", "background: pink; border: 2px solid black");
+item.textContent = "I'm in a div";
+
+// Append our <h1> element to the content <div> node
+content.appendChild(item);
+
+// Create a new item with a <p> tag
+item = document.createElement("p");
+item.textContent = "ME TOO!";
+
+// Append item to content & content to container.
+content.appendChild(item);
 container.appendChild(content);
